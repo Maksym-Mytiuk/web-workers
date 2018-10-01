839d72a48f4e1934cad101620bf1f03705b97a09
Maksym Mytiuk <Maksym.Mytiuk@infopulse.com>
2018-10-01 19:16:42 +0300

    update(*)

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
-window.onload = function () {
-  const inputSearch = document.getElementById('search');
+window.onload = function() {
+  const imageWrapper = document.getElementById('imageWrapper');
+  const btnGetPhotos = document.getElementById('getPhotos');
+  const url = `https://api.unsplash.com/photos/random/?client_id=50c4ae62503d9e1a2cdfe95159d0b512a1d0c70b7e2f6424ea1bb509de9ec3a5`;
 
-  if (!!window.Worker) {
+  if(!!window.Worker) {
+    let counter = 0;
     const worker = new Worker("js/worker.js");
+    const worker2 = new Worker("js/worker.js");
+    const worker3 = new Worker("js/worker.js");
 
-    inputSearch.addEventListener('change', function (e) {
-      worker.postMessage(e.value);
+    btnGetPhotos.addEventListener('click', function() {
+      let interval = setInterval(worker.postMessage(url), 1000);
+      interval();
+      setTimeout(clearInterval(interval), 10000)
     });
 
-    worker.addEventListener('message', function ({data}) {
-      const movieWrapper = document.createElement('div');
-      document.body.appendChild(movieWrapper);
-
+    worker.addEventListener('message', function({data}) {
       const img = document.createElement('img');
       img.src = data.urls.regular;
-      movieWrapper.appendChild(img)
+      imageWrapper.appendChild(img);
+      counter = counter + 1;
+      console.log(counter);
     });
+
+    // worker2.addEventListener('message', function({data}) {
+    //   const img = document.createElement('img');
+    //   img.src = data.urls.regular;
+    //   imageWrapper.appendChild(img);
+    //   console.log('2');
+    // });
+    //
+    // worker3.addEventListener('message', function({data}) {
+    //   const img = document.createElement('img');
+    //   img.src = data.urls.regular;
+    //   imageWrapper.appendChild(img);
+    //   console.log('3');
+    // });
   }
 };
--- js/worker.js
+++ js/worker.js
@@ -1,5 +1,5 @@
-self.addEventListener('message', function () {
-  fetch(`https://api.unsplash.com/photos/random/?client_id=840d21b0d5b76d54ecd48221b246ac3db08f513eedf8f77fc1a40efb4d363ee8`)
+self.addEventListener('message', function({data}) {
+  fetch(data)
     .then(res => res.json())
     .then(data => postMessage(data))
     .catch(error => console.error(error))
